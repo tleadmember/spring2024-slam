db70a08b5b1bce68a2cd4b37b75e3160c8f2f469
Thong Quoc (Bill) Huynh <huynh@mines.edu>
2024-01-27 16:18:15 -0700

    added more examples

--- examples/05_cpp/references.cpp
+++ examples/05_cpp/references.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 using std::cout, std::endl;
 
-int main()
-{
-    int num = 42;
-    int &ref = num;        // reference
-    const int &kRef = num; // const reference
+int main() {
+  int num = 42;
+  int &ref = num;        // reference
+  const int &kRef = num; // const reference
 
-    ref = 0;
-    cout << ref << " " << num << " " << kRef << endl;
-    num = 42;
-    cout << ref << " " << num << " " << kRef << endl;
+  ref = 0;
+  cout << ref << " " << num << " " << kRef << endl;
+  num = 42;
+  cout << ref << " " << num << " " << kRef << endl;
 
-    return 0;
+  return 0;
 }
---
+++ examples/05_cpp/streams.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using std::cerr, std::cin, std::cout;
+
+int main() {
+  cout << "sample std output\n";
+  cerr << "sample error message\n";
+}
