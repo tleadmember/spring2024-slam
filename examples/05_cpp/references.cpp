975723860c23de9567810faa030fe30d19c49691
Thong Quoc (Bill) Huynh <huynh@mines.edu>
2024-01-27 14:38:37 -0700

    add one example about references

---
+++ examples/05_cpp/references.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using std::cout, std::endl;
+
+int main()
+{
+    int num = 42;
+    int &ref = num;        // reference
+    const int &kRef = num; // const reference
+
+    ref = 0;
+    cout << ref << " " << num << " " << kRef << endl;
+    num = 42;
+    cout << ref << " " << num << " " << kRef << endl;
+
+    return 0;
+}
