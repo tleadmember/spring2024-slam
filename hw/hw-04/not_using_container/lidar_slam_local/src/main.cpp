273d2f52f06033e7d2f1331bf5a0c5f165374109
Bill Huynh <huynh@mines.edu>
2024-04-11 17:43:37 -0600

    [feature] complete ICP on RGBD data. Next, continue with ICP on LIDAR, RANSAC on RGBD, RANSAC on LIDAR

--- hw/hw-04/not_using_container/lidar_slam_local/src/main.cpp
+++ hw/hw-04/not_using_container/lidar_slam_local/src/main.cpp
@@ -67,7 +67,7 @@ ptCloudVec visualizeAndGroupPtClouds(const int& num_files,
 
 // Visualize point clouds
 void visualizePtClouds(const int& count,
-                       const ptCloudVec pc_vec) {
+                       const ptCloudVec& pc_vec) {
     for (int i = 0; i < count; ++i) {
         // read point cloud data
         ptCloudPtr point_cloud_ptr = pc_vec[i];
@@ -85,17 +85,51 @@ void visualizePtClouds(const int& count,
 }
 
 // Visualize a registration
-void VisualizeRegistration(const geometry::PointCloud &source,
-                           const geometry::PointCloud &target,
-                           const Eigen::Matrix4d &Transformation) {
+void visualizeRegistration(const geometry::PointCloud& source,
+                           const geometry::PointCloud& target,
+                           const Eigen::Matrix4d& Transformation) {
     ptCloudPtr source_transformed_ptr(new geometry::PointCloud);
     ptCloudPtr target_ptr(new geometry::PointCloud);
     *source_transformed_ptr = source;
     *target_ptr = target;
     source_transformed_ptr->Transform(Transformation);
     visualization::DrawGeometries({source_transformed_ptr, target_ptr},
-                                  "Registration result");
+                                  "Registration result", 1024, 768);
 }
+
+// Register using general ICP
+std::vector<Eigen::Matrix4d> ICPWrapper(const ptCloudVec& pc_vec_down,
+                                        const int& count,
+                                        const double& voxel_size,
+                                        const double& kd_radius_factor,
+                                        const int& max_nn,
+                                        const double& max_corr_dist,
+                                        const int& iterations,
+                                        const double& epsilon) {
+    std::vector<Eigen::Matrix4d> trans_vec;
+    for (int i = 0; i < count; ++i) {
+        trans_vec.emplace_back(Eigen::Matrix4d::Identity()); // initialize
+    }
+    for (int i = 0; i < count-1; ++i) {
+        ptCloudPtr source_down = pc_vec_down[i];
+        ptCloudPtr target_down = pc_vec_down[i+1];
+        source_down->EstimateNormals(geometry::KDTreeSearchParamHybrid(
+                voxel_size * kd_radius_factor, max_nn));
+        target_down->EstimateNormals(geometry::KDTreeSearchParamHybrid(
+                voxel_size * kd_radius_factor, max_nn));
+        auto result = pipelines::registration::RegistrationGeneralizedICP(
+                *source_down, *target_down, max_corr_dist, trans_vec[i],
+                pipelines::registration::TransformationEstimationForGeneralizedICP(),
+                pipelines::registration::ICPConvergenceCriteria(epsilon, epsilon,
+                                                                iterations));
+        trans_vec[i] = result.transformation_;
+        // // visualize the registration above
+        // visualizeRegistration(*source_down, *target_down, trans_vec[i]);
+    }
+
+    return trans_vec;
+}
+
 
 // ----------------------------------------------------------------------------
 int main() {
@@ -131,9 +165,11 @@ int main() {
     // downsample
     ptCloudVec rgbd_pc_vec_down = rgbd_pc_vec; // initialize
     ptCloudVec lidar_pc_vec_down = lidar_pc_vec;
+    const double rgbd_voxel_size = 0.05;
+    const double lidar_voxel_size = 0.4;
     for (int i = 0; i < num_files; ++i) {   
-        rgbd_pc_vec_down[i] = rgbd_pc_vec[i]->VoxelDownSample(0.05);
-        lidar_pc_vec_down[i] = lidar_pc_vec[i]->VoxelDownSample(0.1);
+        rgbd_pc_vec_down[i] = rgbd_pc_vec[i]->VoxelDownSample(rgbd_voxel_size);
+        lidar_pc_vec_down[i] = lidar_pc_vec[i]->VoxelDownSample(lidar_voxel_size);
     }
 
     // // visualize point clouds after downsampling
@@ -141,34 +177,42 @@ int main() {
     // visualizePtClouds(num_files, lidar_pc_vec_down);
 
     // register rgbd point clouds using ICP
-    std::vector<Eigen::Matrix4d> trans_vec;
-    for (int i = 0; i < num_files; ++i) {
-        trans_vec.emplace_back(Eigen::Matrix4d::Identity()); // initialize)
+    const double rgbd_kd_radius_factor = 4.0;
+    const int rgbd_max_nn = 30;
+    const double rgbd_max_corr_dist = 1.0;
+    const int rgbd_iterations = 100;
+    const double rgbd_epsilon = 1e-6;
+    std::vector<Eigen::Matrix4d> trans_vec = ICPWrapper(rgbd_pc_vec_down,
+                                                        num_files,
+                                                        rgbd_voxel_size,
+                                                        rgbd_kd_radius_factor,
+                                                        rgbd_max_nn,
+                                                        rgbd_max_corr_dist,
+                                                        rgbd_iterations,
+                                                        rgbd_epsilon);
+    // transform point clouds (into final frame, instead of 0-frame)
+    Eigen::Matrix4d trans_accum = Eigen::Matrix4d::Identity();
+    for (int i = num_files-1; i > 0; --i) {
+        // rgbd_pc_vec[i-1] = 
+        trans_accum = trans_vec[i-1] * trans_accum;
+        rgbd_pc_vec_down[i-1]->Transform(trans_accum);
     }
-    for (int i = 0; i < num_files-1; ++i) {
-        ptCloudPtr source_down = rgbd_pc_vec_down[i];
-        ptCloudPtr target_down = rgbd_pc_vec_down[i+1];
+    // fuse point clouds and visualize
+    ptCloudPtr pc_combined_ptr(new geometry::PointCloud);
+    for (int i = 0; i < num_files; ++i) {   
+        *pc_combined_ptr += *rgbd_pc_vec_down[i];
     }
-    
-    // visualize the registration above
-
+    visualization::DrawGeometries({pc_combined_ptr},
+                                  "Combined registration result (ICP on RGBD data)", 1024, 768);
 
     // register lidar point clouds using ICP
-
-
-    // visualize the registration above
 
 
     // register rgbd point clouds using RANSAC with FPFH correspondence
 
 
-    // visualize the registration above
-
-
     // register lidar point clouds using RANSAC with FPFH correspondence
 
-
-    // visualize the registration above
 
 
     return 0;
